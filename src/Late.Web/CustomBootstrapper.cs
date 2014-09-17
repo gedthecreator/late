0861a01ac95a706e67acd30fccfb12ab883bfdb2
gedthecreator <macdermott@gmail.com>
2014-09-17 17:19:36 +0100

    Please work

--- src/Late.Web/CustomBootstrapper.cs
+++ src/Late.Web/CustomBootstrapper.cs
@@ -10,8 +10,9 @@ namespace Late.Web
     {
         protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
         {
-            container.Register<IMobileWebClient>(new MobileWebClient());
             StaticConfiguration.DisableErrorTraces = false;
+
+            container.Register<IMobileWebClient>(new MobileWebClient());
 
             base.ApplicationStartup(container, pipelines);
         }
